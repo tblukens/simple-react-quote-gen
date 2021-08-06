ed8544d883435befef5159a9f5a7d692b8903920
tlukens <tblukens87@gmail.com>
2021-08-06 16:37:30 -0500

    Push to git for github pages check

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+const QuoteBox = () => {
+	const [quote, setQuote] = React.useState({})
+
+	const getQuote = () => {
+		fetch('https://api.quotable.io/random?tags=famous-quotes')
+			.then(res => res.json())
+			.then(data => setQuote(data))
+			.catch(err => console.log(err))
+	}
+
+	React.useEffect(() => {
+		getQuote()
+	}, [])
+
+	return (
+		<div id='quote-box'>
+			<div id='text'>{quote.content}</div>
+			<div id='author'>{quote.author}</div>
+			<div id='col1'>
+				<a id='tweet-quote' class='bttn-bordered bttn-xs bttn-default'>
+					Tweet It!
+				</a>
+				<button
+					id='new-quote'
+					class='bttn-bordered bttn-xs bttn-default'
+					onClick={getQuote}
+				>
+					New Quote
+				</button>
+			</div>
+		</div>
+	)
+}
+
+ReactDOM.render(<QuoteBox />, document.getElementById('root'))
